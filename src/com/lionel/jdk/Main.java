053e39510ab501a7b9d4d4a8c4077f9e67c27428
李远龙 <614976970@qq.com>
2021-03-10 19:45:01 +0800

    initial

---
+++ src/com/lionel/jdk/Main.java
@@ -0,0 +1,14 @@
+package com.lionel.jdk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+		Map<String, String> map = new HashMap<>();
+		map.put("1", "lionel");
+		map.put("2", "haha");
+    }
+}
